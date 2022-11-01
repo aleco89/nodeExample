e037f43c02cdb5fd37630ad7a920298552c74075
aleco89 <analeconte89@gmail.com>
2022-10-31 21:55:16 -0300

    correccion endpoints

--- index.ts
+++ index.ts
@@ -5,8 +5,20 @@ const app = express();
 
 app.use(bodyParser.json(), urlencoded({ extended: true }));
 app.use(cors());
+// tb puedo crear una variable allowedOrigins y declarar los sitios desde los cuales voy a acceder
 
-const product: { id: number; name: string; marca: string }[] = [];
+const product: { id: number; name: string; marca: string }[] = [
+  {
+    name: "Teclado",
+    marca: "Logitech",
+    id: 2131231,
+  },
+  {
+    name: "Placa",
+    marca: "AMD",
+    id: 21312312,
+  },
+];
 
 app.get("/products", (req, res) => {
   res.status(200).json(product);
@@ -17,26 +29,31 @@ Libreria body-parser resuelve problemas al interpretar el body
 
 app.post("/products", (req, res) => {
   const { name, marca } = req.body;
-  try {
-    if (!name || !marca) throw new Error("che pasame el name y marca");
-    product.push({ id: new Date().getTime(), name, marca });
-    res.status(201).json(...product.slice(-1));
-  } catch (err) {
-    return res.status(400).json({ message: err.message });
-  }
+  if (!name || !marca)
+    return res
+      .status(400)
+      .json({ message: "fields 'name' and 'marca' are requiered" });
+  product.push({ id: new Date().getTime(), name, marca });
+  return res.status(200).json(...product.slice(-1));
 });
 
-app.put("/products", (req, res) => {
-  const { name, marca, id } = req.body;
+app.put("/products/:id", (req, res) => {
+  const { name, marca } = req.body;
+  const id = Number(req.params.id);
   const index = product.findIndex((product) => product.id === id);
   if (index === -1)
-    return res.status(400).json({ message: "no se encontró el producto" });
+    return res.status(400).json({ message: "product not found" });
   product[index] = { ...product[index], name, marca };
   res.status(200).json(product[index]);
 });
 
-app.delete("/products", (req, res) => {
-  res.status(200).json({ name: "Ana" });
+app.delete("/product/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = product.findIndex((product) => product.id === id);
+  if (index === -1)
+    return res.status(400).json({ message: "product not found" });
+
+  return res.status(200).json(product.splice(index, 1));
 });
 
 app.listen(3000);
